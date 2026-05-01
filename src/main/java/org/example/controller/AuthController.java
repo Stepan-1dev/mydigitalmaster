0d6e71b2cce97b04aa7c3715177a5b8833fd6449
pidanov <spp9@tpu.ru>
2026-05-01 19:08:07 +0700

    add test GetMapping

--- src/main/java/org/example/controller/AuthController.java
+++ src/main/java/org/example/controller/AuthController.java
@@ -3,6 +3,7 @@ import org.example.entity.AuthInfoForExchange;
 import org.example.entity.ExchangeStatus;
 import org.example.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +21,10 @@ public class AuthController {
     @PostMapping("/api/vk/exchange")
     public ExchangeStatus exhangeCodeToTokens(@RequestBody AuthInfoForExchange authInfoForExchange){
         return authService.echangeCodeToTokens(authInfoForExchange);
+    }
+
+    @GetMapping("/api/ping")
+    public String Testik(){
+        return "pong";
     }
 }
