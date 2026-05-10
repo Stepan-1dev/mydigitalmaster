28411fbdb3e48416c518365784989f0d6d6fff3e
pidanov <spp9@tpu.ru>
2026-05-10 13:11:19 +0700

    added UserProfileEntity

--- src/main/java/org/example/entity/UserProfileEntity.java
+++ src/main/java/org/example/entity/UserProfileEntity.java
@@ -1,4 +1,77 @@
 package org.example.entity;
 
+import jakarta.persistence.*;
+
+@Table(name = "usersprofile")
+@Entity
 public class UserProfileEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    @Column(name = "user_vk_id")
+    private Long userVkId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "avatar")
+    private String avatar;
+
+    @Column(name = "sex")
+    private String sex;
+
+    public UserProfileEntity() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserVkId() {
+        return userVkId;
+    }
+
+    public void setUserVkId(Long userVkId) {
+        this.userVkId = userVkId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
 }
